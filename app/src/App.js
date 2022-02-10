84a94c729b7dba7f724e65fdcc104d68c0473bc3
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-02-10 17:38:09 +0100

    #11 Update NextUI

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { CssBaseline, ThemeProvider } from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
-    <ThemeProvider theme={{ type: "dark" }}>
-    <CssBaseline />
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </ThemeProvider>
+    <NextUIProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </NextUIProvider>
   );
 }
 

3fb9b0da47c86de1f004c6f1ce30c69e8faecd1d
Sandra Seville <sandrahs@stud.ntnu.no>
2022-02-17 16:24:45 +0100

    Provd aa legge inn Theme

---
+++ app/src/components/CreateEvent/index.js
@@ -0,0 +1,55 @@
+import { Button, Input, Text, Textarea } from '@nextui-org/react';
+import './style.css';
+
+const CreateEvent = () => {
+  return (
+    <div className="CreateEvent">
+      <Text
+        h1
+        size={40}
+        color="primary"
+        weight="bold"
+        fonts="overskrift"
+        className="header"
+      >
+        Opprett arrangement
+      </Text>
+
+      <form method="POST" action="http://localhost:8000/event">
+        <div className="inputs">
+          <Input
+            type="text"
+            name="title"
+            labelPlaceholder="Tittel"
+            clearable
+            underlined
+            required
+          />
+          <Input
+            type="text"
+            name="location"
+            labelPlaceholder="Sted"
+            clearable
+            underlined
+            required
+          />
+          <Input type="date" name="date" underlined required />
+          <Button type="submit" color="primary">
+            Opprett
+          </Button>
+        </div>
+
+        <Textarea
+          name="description"
+          labelPlaceholder="Arrangementbeskrivelse"
+          helperText="Husk å legg til kontaktinformasjon"
+          className="textarea"
+          bordered
+          required
+        />
+      </form>
+    </div>
+  );
+};
+
+export default CreateEvent;

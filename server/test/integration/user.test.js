69a6cccd5306639911f7129f095a8133261a0888
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-02-17 20:19:41 +0100

    #15 Made event integration tests and cleaned user tests

--- server/test/integration/user.test.js
+++ server/test/integration/user.test.js
@@ -5,55 +5,70 @@ const mockDataBase = require('../utils/mockDataBase');
 
 let request;
 
-beforeAll(async () => {
-  //Initiate the database with users
-  const user = new User({
+const exampleUsers = [
+  {
     username: 'groupie',
-  });
-  user.save();
-  const user2 = new User({
+  },
+  {
     username: 'groupette',
-  });
-  user2.save();
+  },
+];
 
+beforeAll(async () => {
   mockDataBase.connect();
   request = supertest(app);
+});
+
+beforeEach(async () => {
+  //Initiate the database with users
+  exampleUsers.forEach((user) => new User(user).save());
 });
 
 afterAll(async () => {
   mockDataBase.disconnect();
 });
 
-it('returns all users with GET /user', async () => {
-  await request
-    .get('/user')
-    .expect(200)
-    .then((res) => {
-      //Check that we get an array with only one user
-      expect(Array.isArray(res.body)).toBeTruthy();
-      expect(res.body.length).toEqual(2);
-      //Check that it's the correct user
-      expect(res.body[0].username).toBe('groupie');
-      expect(res.body[1].username).toBe('groupette');
-    });
+afterEach(async () => {
+  //Delete all events
+  User.deleteMany({});
 });
+describe('the user route', () => {
+  it('returns all users with GET /user', async () => {
+    await request
+      .get('/user')
+      .expect(200)
+      .then((res) => {
+        //Check that we get an array with the correct length
+        expect(Array.isArray(res.body)).toBeTruthy();
+        expect(res.body.length).toEqual(exampleUsers.length);
+        //Check for the correct users
+        exampleUsers.forEach((exampleUser) =>
+          res.body
+            .filter((resUser) => resUser.username === exampleUser.username)
+            .forEach((resUser) => {
+              expect(resUser.username).toBe(exampleUser.username);
+            })
+        );
+      });
+  });
 
-it('adds a user to the database with POST /user', async () => {
-  const data = {
-    username: 'grupert',
-  };
-  await request
-    .post('/user')
-    .send(data)
-    .expect(200)
-    .then(async (res) => {
-      //Check that the response is correct
-      expect(res.body._id).toBeTruthy();
-      expect(res.body.username).toBe(data.username);
+  it('adds a user to the database with POST /user', async () => {
+    const data = {
+      username: 'grupert',
+    };
+    await request
+      .post('/user')
+      .send(data)
+      .expect(200)
+      .then(async (res) => {
+        //Check that the response is correct
+        expect(res.body._id).toBeTruthy();
+        expect(res.body.username).toBe(data.username);
 
-      //Check that the user is in the database
-      const user = await User.findOne({ _id: res.body._id });
-      expect(user).toBeTruthy();
-      expect(user.username).toBe(data.username);
-    });
+        //Check that the user is in the database
+        const user = await User.findOne({ _id: res.body._id });
+        expect(user).toBeTruthy();
+        expect(user.username).toBe(data.username);
+      });
+  });
 });

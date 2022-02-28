d70c6d574b9a81f06f6c1be07f2ca49f630f759f
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-02-28 10:26:49 +0100

    #22 Moved dotenv import

--- server/middleware/cloudinary.config.js
+++ server/middleware/cloudinary.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');

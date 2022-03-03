0cb0c2a6e2f469d3e4405cde9c65158b2e095d25
Ivar Nakken <ivarnakken1441@gmail.com>
2022-03-03 09:50:36 +0100

    Add content to footer

--- app/src/components/Footer/index.js
+++ app/src/components/Footer/index.js
@@ -1,15 +1,36 @@
-import { Text, Col, Row } from '@nextui-org/react';
+import { Text, Spacer } from '@nextui-org/react';
 import './style.css';
 
 const Footer = () => {
   return (
     <footer>
       <div className="main">
-        <Row>
-          <Col>
-            <Text>Kontakt oss</Text>
-          </Col>
-        </Row>
+        <div className="soMe">
+          <box-icon type="logo" name="snapchat" color="#000000AA" />
+          <Spacer x={2} />
+          <box-icon type="logo" name="instagram-alt" color="#000000AA" />
+          <Spacer x={2} />
+          <box-icon type="logo" name="twitter" color="#000000AA" />
+          <Spacer x={2} />
+          <box-icon type="logo" name="facebook" color="#000000AA" />
+        </div>
+        <div className="info-text">
+          <Text h5 weight="medium" color="#000000AA">
+            Tjenester
+          </Text>
+          <Spacer x={3} />
+          <Text h5 weight="medium" color="#000000AA">
+            Om oss
+          </Text>
+          <Spacer x={3} />
+          <Text h5 weight="medium" color="#000000AA">
+            Vilkår
+          </Text>
+          <Spacer x={3} />
+          <Text h5 weight="medium" color="#000000AA">
+            Personvernerklæring
+          </Text>
+        </div>
       </div>
       <div className="bottom">
         <span>© 2022 GroupUp. All rights reserved.</span>

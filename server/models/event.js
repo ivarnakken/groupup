f7cda42f61710e80ef99700cb1231c4f3cb23764
sondreas <sondreas@stud.ntnu.no>
2022-03-18 13:47:07 +0100

    La til min/max-lengde på inputfelter (#16)

--- server/models/event.js
+++ server/models/event.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  location: { type: String, required: true },
+  title: { type: String, minlength: 1, maxlength: 30, required: true },
+  location: { type: String, minlength: 1, maxlength: 100, required: true },
   date: { type: Date, required: true },
-  description: { type: String, required: true },
+  description: { type: String, minlength: 1, maxlength: 400, required: true },
   tags: { type: [String], required: false },
   image: { type: String },
   group: {

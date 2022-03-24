2dfc7bf71fa7db2945f91528899cf99fd4799c9d
Sandra Seville <sandrahs@stud.ntnu.no>
2022-03-24 12:43:38 +0100

    brukermanual i boksene

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -18,9 +18,9 @@ const Home = () => {
 
   return (
     <div className="home">
-      <div className="top-content">
-        <div className="text">
-          <Col className="info">
+      <div className='top-content'>
+      <div className="text">
+        <Col lg={5} sm={5} className='info'>
             <Text h1 size={60} weight="bold" with="100">
               Velkommen til
             </Text>
@@ -47,228 +47,232 @@ const Home = () => {
                 Registrer deg
               </Button>
             )}
-          </Col>
-        </div>
-
-        <Col lg={3} sm={4} className="groupImg">
-          <Card cover>
-            <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-              <Col>
-                <Text
-                  size={12}
-                  weight="bold"
-                  transform="uppercase"
-                  color="#primary"
-                >
-                  Føler du et behov for å gjøre noe annet enn de faste mønstrene
-                  av aktiviteter med de samme menneskene?
-                </Text>
-              </Col>
-            </Card.Header>
-            <Card.Image
-              src="groupsun.png"
-              height={400}
-              width="100%"
-              alt="Card image background"
-            />
-          </Card>
-        </Col>
+            </Col>
+            <Grid.Container gap={1}>
+        <Grid>
+          <Avatar text="JR" size="xs" />
+        </Grid>
+        <Grid>
+          <Avatar src="soga.jpg" size="sm" />
+        </Grid>
+        <Grid>
+          <Avatar text="Joe" size="md" />
+        </Grid>
+        <Grid>
+          <Avatar src="meg.jpg" size="lg" />
+        </Grid>
+        <Grid>
+          <Avatar text="Deg?" size="xl" />
+        </Grid>
+        <Grid>
+          <Avatar src="galla.jpg" css={{ size: '$20' }} />
+        </Grid>
+      </Grid.Container>
       </div>
+          <Spacer x={10}></Spacer>
+          <Col lg={3} sm={5} className='groupImg' justify='right'>
+              <Card cover>
+                <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+                  <Col>
+                    <Text
+                      size={12}
+                      weight="bold"
+                      transform="uppercase"
+                      color="#primary"
+                    >
+                      Føler du et behov for å gjøre noe annet enn de faste
+                      mønstrene av aktiviteter med de samme menneskene?
+                    </Text>
+                  </Col>
+                </Card.Header>
+                <Card.Image
+                  src="groupsun.png"
+                  height={400}
+                  width="100%"
+                  alt="Card image background"
+                />
+              </Card>
+            </Col>
+            </div>
+
+<Spacer y={10} />
+
+<div className='content'>
+
+      <div>
+        <Spacer y={30} x={50}></Spacer>
+
+      <Grid.Container>
+        <Grid.Container gap={4} justify="center">
+          <Row>
+            <Grid sm={12} md={10} justify="left">
+              <Card css={{ mw: '330px' }}>
+                <Card.Header>
+                  <Text b>MØTEPLASS</Text>
+                </Card.Header>
+                <Divider />
+                <Card.Body css={{ py: '$10' }}>
+                  <Text>
+                    GroupUp er en plattform hvor vennegjenger skal kunne møtes
+                    for å dyrke felles interesser og bli kjent.
+                  </Text>
+                </Card.Body>
+                <Divider />
+                <Card.Footer>
+                  <Row justify="flex-end"></Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
+            <Grid sm={12} md={10} justify="left">
+              <Card css={{ mw: '330px' }}>
+                <Card.Header>
+                  <Text b>GRUPPER</Text>
+                </Card.Header>
+                <Divider />
+                <Card.Body css={{ py: '$10' }}>
+                  <Text>
+                    Brukere kan opprette grupper, hvor de kan legge til vennene
+                    sine. En gruppe består av én eller flere personlige
+                    profiler. Gruppene må matche med hverandre for at et møte
+                    skal skje, og kan kommunisere med hverandre. Etter hvert som
+                    det avtales møter, kan gruppene kunne se en liste over
+                    matcher med andre grupper.
+                  </Text>
+                </Card.Body>
+                <Divider />
+                <Card.Footer>
+                  <Row justify="flex-end"></Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
+            <Spacer></Spacer>
+            <Grid sm={12} md={10} justify="left">
+              <Card css={{ mw: '330px' }}>
+                <Card.Header>
+                  <Text b>INTERESSER ++</Text>
+                </Card.Header>
+                <Divider />
+                <Card.Body css={{ py: '$10' }}>
+                  <Text>
+                    En gruppe kan legge til sine interesser, ha en beskrivelse,
+                    antall i gruppa, aldersspenn, bilde og dato for ønsket
+                    aktivitet. Grupper kan se alle andre grupper, og filtrere
+                    dem på interesser, lokasjon, alder, gruppestørrelse og dato
+                    før ønsket møte.
+                  </Text>
+                </Card.Body>
+                <Divider />
+                <Card.Footer></Card.Footer>
+              </Card>
+            </Grid>
+            <Spacer></Spacer>
+            <Grid sm={12} md={10} justify="left">
+              <Card css={{ mw: '330px' }}>
+                <Card.Header>
+                  <Text b>HYGGELIGE OPPLEVELSER</Text>
+                </Card.Header>
+                <Divider />
+                <Card.Body css={{ py: '$10' }}>
+                  <Text>
+                    For å sikre hyggelige opplevelser på plattformen kan grupper
+                    gi en review til hverandre etter møtedato. Videre, for å
+                    sikre seriøse brukere, må en logge seg inn med passord på
+                    egen bruker for å delta på forumet. Brukere kan rapportere
+                    andre brukere som bryter retningslinjene. Dersom sidens
+                    administrator oppdager brudd eller svindel, kan
+                    administrator fjerne upassende grupper og slette brukere.
+                  </Text>
+                </Card.Body>
+                <Divider />
+                <Card.Footer>
+                  <Row justify="flex-end"></Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
+            <Spacer y={3}></Spacer>
+          </Row>
+        </Grid.Container>
 
       <Spacer y={2} />
 
-      <div className="content">
-        <Grid.Container gap={1}>
-          <Grid>
-            <Avatar text="JR" size="xs" />
-          </Grid>
-          <Grid>
-            <Avatar src="soga.jpg" size="sm" />
-          </Grid>
-          <Grid>
-            <Avatar text="Joe" size="md" />
-          </Grid>
-          <Grid>
-            <Avatar src="meg.jpg" size="lg" />
-          </Grid>
-          <Grid>
-            <Avatar text="Deg?" size="xl" />
-          </Grid>
-          <Grid>
-            <Avatar src="galla.jpg" css={{ size: '$20' }} />
+        <Grid.Container gap={4} justify="center">
+          <Grid lg={11} sm={10}>
+            <Card cover>
+              <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+                <Col>
+                  <Text
+                    size={12}
+                    weight="bold"
+                    transform="uppercase"
+                    color="#primary"
+                  >
+                    Tur?
+                  </Text>
+                </Col>
+              </Card.Header>
+              <Card.Image
+                src="tur.jpg"
+                height={400}
+                width="100%"
+                alt="Card image background"
+              />
+            </Card>
           </Grid>
         </Grid.Container>
 
-        <Spacer y={2} />
-
-        <Grid.Container>
-          <Grid.Container gap={4} justify="center">
-            <Row>
-              <Grid sm={12} md={10} justify="left">
-                <Card css={{ mw: '330px' }}>
-                  <Card.Header>
-                    <Text b>MØTEPLASS</Text>
-                  </Card.Header>
-                  <Divider />
-                  <Card.Body css={{ py: '$10' }}>
-                    <Text>
-                      GroupUp er en plattform hvor vennegjenger skal kunne møtes
-                      for å dyrke felles interesser og bli kjent.
-                    </Text>
-                  </Card.Body>
-                  <Divider />
-                  <Card.Footer>
-                    <Row justify="flex-end"></Row>
-                  </Card.Footer>
-                </Card>
-              </Grid>
-              <Grid sm={12} md={10} justify="left">
-                <Card css={{ mw: '330px' }}>
-                  <Card.Header>
-                    <Text b>GRUPPER</Text>
-                  </Card.Header>
-                  <Divider />
-                  <Card.Body css={{ py: '$10' }}>
-                    <Text>
-                      Brukere kan opprette grupper, hvor de kan legge til
-                      vennene sine. En gruppe består av én eller flere
-                      personlige profiler. Gruppene må matche med hverandre for
-                      at et møte skal skje, og kan kommunisere med hverandre.
-                      Etter hvert som det avtales møter, kan gruppene kunne se
-                      en liste over matcher med andre grupper.
-                    </Text>
-                  </Card.Body>
-                  <Divider />
-                  <Card.Footer>
-                    <Row justify="flex-end"></Row>
-                  </Card.Footer>
-                </Card>
-              </Grid>
-              <Spacer></Spacer>
-              <Grid sm={12} md={10} justify="left">
-                <Card css={{ mw: '330px' }}>
-                  <Card.Header>
-                    <Text b>INTERESSER ++</Text>
-                  </Card.Header>
-                  <Divider />
-                  <Card.Body css={{ py: '$10' }}>
-                    <Text>
-                      En gruppe kan legge til sine interesser, ha en
-                      beskrivelse, antall i gruppa, aldersspenn, bilde og dato
-                      for ønsket aktivitet. Grupper kan se alle andre grupper,
-                      og filtrere dem på interesser, lokasjon, alder,
-                      gruppestørrelse og dato før ønsket møte.
-                    </Text>
-                  </Card.Body>
-                  <Divider />
-                  <Card.Footer></Card.Footer>
-                </Card>
-              </Grid>
-              <Spacer></Spacer>
-              <Grid sm={12} md={10} justify="left">
-                <Card css={{ mw: '330px' }}>
-                  <Card.Header>
-                    <Text b>HYGGELIGE OPPLEVELSER</Text>
-                  </Card.Header>
-                  <Divider />
-                  <Card.Body css={{ py: '$10' }}>
-                    <Text>
-                      For å sikre hyggelige opplevelser på plattformen kan
-                      grupper gi en review til hverandre etter møtedato. Videre,
-                      for å sikre seriøse brukere, må en logge seg inn med
-                      passord på egen bruker for å delta på forumet. Brukere kan
-                      rapportere andre brukere som bryter retningslinjene.
-                      Dersom sidens administrator oppdager brudd eller svindel,
-                      kan administrator fjerne upassende grupper og slette
-                      brukere.
-                    </Text>
-                  </Card.Body>
-                  <Divider />
-                  <Card.Footer>
-                    <Row justify="flex-end"></Row>
-                  </Card.Footer>
-                </Card>
-              </Grid>
-              <Spacer y={3}></Spacer>
-            </Row>
-          </Grid.Container>
-
-          <Grid.Container gap={4} justify="center">
-            <Grid lg={11} sm={10}>
-              <Card cover>
-                <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-                  <Col>
-                    <Text
-                      size={12}
-                      weight="bold"
-                      transform="uppercase"
-                      color="#primary"
-                    >
-                      Tur?
-                    </Text>
-                  </Col>
-                </Card.Header>
-                <Card.Image
-                  src="tur.jpg"
-                  height={400}
-                  width="100%"
-                  alt="Card image background"
-                />
-              </Card>
-            </Grid>
-          </Grid.Container>
-
-          <Grid.Container gap={4} justify="center">
-            <Grid lg={11} sm={10}>
-              <Card cover>
-                <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-                  <Col>
-                    <Text
-                      size={12}
-                      weight="bold"
-                      transform="uppercase"
-                      color="#primary"
-                    >
-                      Kajakk?
-                    </Text>
-                  </Col>
-                </Card.Header>
-                <Card.Image
-                  src="kajakk.jpg"
-                  height={400}
-                  width="100%"
-                  alt="Card image background"
-                />
-              </Card>
-            </Grid>
-          </Grid.Container>
-
-          <Grid.Container gap={4} justify="center">
-            <Grid lg={11} sm={10}>
-              <Card cover>
-                <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-                  <Col>
-                    <Text
-                      size={12}
-                      weight="bold"
-                      transform="uppercase"
-                      color="#primary"
-                    >
-                      Hyttetur?
-                    </Text>
-                  </Col>
-                </Card.Header>
-                <Card.Image
-                  src="hyttetur.jpg"
-                  height={400}
-                  width="100%"
-                  alt="Card image background"
-                />
-              </Card>
-            </Grid>
-          </Grid.Container>
+        <Grid.Container gap={4} justify="center">
+          <Grid lg={11} sm={10}>
+            <Card cover>
+              <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+                <Col>
+                  <Text
+                    size={12}
+                    weight="bold"
+                    transform="uppercase"
+                    color="#primary"
+                  >
+                    Kajakk?
+                  </Text>
+                </Col>
+              </Card.Header>
+              <Card.Image
+                src="kajakk.jpg"
+                height={400}
+                width="100%"
+                alt="Card image background"
+              />
+            </Card>
+          </Grid>
         </Grid.Container>
-      </div>
+
+        <Grid.Container gap={4} justify="center">
+          <Grid lg={11} sm={10}>
+            <Card cover>
+              <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+                <Col>
+                  <Text
+                    size={12}
+                    weight="bold"
+                    transform="uppercase"
+                    color="#primary"
+                  >
+                    Hyttetur?
+                  </Text>
+                </Col>
+              </Card.Header>
+              <Card.Image
+                src="hyttetur.jpg"
+                height={400}
+                width="100%"
+                alt="Card image background"
+              />
+            </Card>
+          </Grid>
+        </Grid.Container>
+      </Grid.Container>
+    </div>
+
+            </div>
     </div>
   );
 };

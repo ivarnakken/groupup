d06e4ee3c0d35872fe2ef7ba4395e60f03b0e962
Ivar Nakken <ivarnakken1441@gmail.com>
2022-03-27 19:01:32 +0200

    Style the entire home page

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -1,4 +1,4 @@
-import { Button, Text, Col, Grid, Card, Spacer, Row } from '@nextui-org/react';
+import { Button, Text, Col, Card, Row, Spacer } from '@nextui-org/react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './style.css';
@@ -8,7 +8,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      <Row align="center" wrap="wrap">
+      <Row align="center" wrap="wrap" className="top">
         <Col className="headerInfo">
           <Row justify="flex-start">
             <Text className="styledTitle" css={{ mb: 0 }}>
@@ -62,21 +62,13 @@ const Home = () => {
           )}
         </Col>
 
+        <img
+          src="gradient1.svg"
+          style={{ position: 'absolute', marginLeft: '200px' }}
+        />
+
         <div className="groupImg">
           <Card cover>
-            <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-              <Col>
-                <Text
-                  size={12}
-                  weight="bold"
-                  transform="uppercase"
-                  color="#primary"
-                >
-                  Føler du et behov for å gjøre noe annet enn de faste mønstrene
-                  av aktiviteter med de samme menneskene?
-                </Text>
-              </Col>
-            </Card.Header>
             <Card.Image
               src="groupsun.png"
               height={400}
@@ -87,155 +79,120 @@ const Home = () => {
         </div>
       </Row>
 
-      <Spacer y={11} />
-
       <div className="manual">
-        <Grid.Container gap={4} justify="center">
-          <Row>
-            <Grid sm={12} md={10} justify="left">
-              <Card css={{ mw: '330px' }} className="infoCard">
-                <Card.Header>
-                  <Text b>MØTEPLASS</Text>
-                </Card.Header>
-                <Card.Body css={{ py: '$10' }}>
-                  <Text>
-                    GroupUp er en plattform hvor vennegjenger skal kunne møtes
-                    for å dyrke felles interesser og bli kjent.
-                  </Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Row justify="flex-end"></Row>
-                </Card.Footer>
-              </Card>
-            </Grid>
-            <Grid sm={12} md={10} justify="left">
-              <Card css={{ mw: '330px' }} className="infoCard">
-                <Card.Header>
-                  <Text b>GRUPPER</Text>
-                </Card.Header>
-                <Card.Body css={{ py: '$10' }}>
-                  <Text>
-                    Brukere kan opprette grupper, hvor de kan legge til vennene
-                    sine. En gruppe består av én eller flere personlige
-                    profiler. Gruppene må matche med hverandre for at et møte
-                    skal skje, og kan kommunisere med hverandre. Etter hvert som
-                    det avtales møter, kan gruppene kunne se en liste over
-                    matcher med andre grupper.
-                  </Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Row justify="flex-end"></Row>
-                </Card.Footer>
-              </Card>
-            </Grid>
-            <Spacer></Spacer>
-            <Grid sm={12} md={10} justify="left">
-              <Card css={{ mw: '330px' }} className="infoCard">
-                <Card.Header>
-                  <Text b>INTERESSER ++</Text>
-                </Card.Header>
-                <Card.Body css={{ py: '$10' }}>
-                  <Text>
-                    En gruppe kan legge til sine interesser, ha en beskrivelse,
-                    antall i gruppa, aldersspenn, bilde og dato for ønsket
-                    aktivitet. Grupper kan se alle andre grupper, og filtrere
-                    dem på interesser, lokasjon, alder, gruppestørrelse og dato
-                    før ønsket møte.
-                  </Text>
-                </Card.Body>
-                <Card.Footer></Card.Footer>
-              </Card>
-            </Grid>
-            <Spacer />
-            <Grid sm={12} md={10} justify="left">
-              <Card css={{ mw: '330px' }} className="infoCard">
-                <Card.Header>
-                  <Text b>HYGGELIGE OPPLEVELSER</Text>
-                </Card.Header>
-                <Card.Body css={{ py: '$10' }}>
-                  <Text>
-                    For å sikre hyggelige opplevelser på plattformen kan grupper
-                    gi en review til hverandre etter møtedato. Videre, for å
-                    sikre seriøse brukere, må en logge seg inn med passord på
-                    egen bruker for å delta på forumet. Brukere kan rapportere
-                    andre brukere som bryter retningslinjene. Dersom sidens
-                    administrator oppdager brudd eller svindel, kan
-                    administrator fjerne upassende grupper og slette brukere.
-                  </Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Row justify="flex-end"></Row>
-                </Card.Footer>
-              </Card>
-            </Grid>
-            <Spacer y={3} />
-          </Row>
-        </Grid.Container>
-
-        <Spacer y={2} />
-
-        <Card cover>
-          <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-            <Col>
-              <Text
-                size={12}
-                weight="bold"
-                transform="uppercase"
-                color="#primary"
-              >
-                Tur?
+        <img
+          src="gradient2.svg"
+          style={{
+            position: 'absolute',
+            width: '1100px',
+            marginBottom: '1000px',
+          }}
+        />
+        <div className="section">
+          <div className="textSection">
+            <Row justify="flex-start">
+              <Text className="styledTitle" css={{ mb: 0 }}>
+                Opprett og&nbsp;
               </Text>
-            </Col>
-          </Card.Header>
-          <Card.Image
-            src="tur.jpg"
-            height={400}
-            width="100%"
-            alt="Card image background"
-          />
-        </Card>
-
-        <Card cover>
-          <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-            <Col>
               <Text
-                size={12}
-                weight="bold"
-                transform="uppercase"
-                color="#primary"
+                className="styledTitle"
+                css={{
+                  mb: 0,
+                  textGradient: '180deg, #53F76E 20%, #2CA340 100%',
+                }}
               >
-                Kajakk?
+                match&nbsp;
               </Text>
-            </Col>
-          </Card.Header>
-          <Card.Image
-            src="kajakk.jpg"
-            height={400}
-            width="100%"
-            alt="Card image background"
-          />
-        </Card>
-
-        <Card cover>
-          <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-            <Col>
+            </Row>
+            <Row justify="flex-start">
               <Text
-                size={12}
-                weight="bold"
-                transform="uppercase"
-                color="#primary"
+                className="styledTitle"
+                css={{ mb: 0, '@xsMax': { d: 'inline-block' } }}
               >
-                Hyttetur?
+                med grupper.
               </Text>
-            </Col>
-          </Card.Header>
-          <Card.Image
-            src="hyttetur.jpg"
-            height={400}
-            width="100%"
-            alt="Card image background"
+            </Row>
+
+            <Text
+              h4
+              color="#666"
+              weight="medium"
+              css={{ mt: 30 }}
+              className="comment"
+            >
+              Brukere kan opprette grupper med sine nærmeste. Match deretter med
+              andre grupper gjennom arrangementer.
+            </Text>
+          </div>
+
+          <Card cover>
+            <Card.Image
+              src="tur.jpg"
+              height={400}
+              width="100%"
+              alt="Gruppe på tur"
+            />
+          </Card>
+        </div>
+
+        <div className="section">
+          <Card cover>
+            <Card.Image
+              src="hyttetur.jpg"
+              height={400}
+              width="100%"
+              alt="En perfekt match mellom"
+            />
+          </Card>
+
+          <img
+            src="gradient3.svg"
+            style={{
+              position: 'absolute',
+              width: '900px',
+              marginTop: '-400px',
+              marginLeft: '400px',
+            }}
           />
-        </Card>
+
+          <div className="textSection">
+            <Row justify="flex-start">
+              <Text className="styledTitle" css={{ mb: 0 }}>
+                Den&nbsp;
+              </Text>
+              <Text
+                className="styledTitle"
+                css={{
+                  mb: 0,
+                  textGradient: '180deg, #E4441C 40%, #AA2B0C 100%',
+                }}
+              >
+                perfekte&nbsp;
+              </Text>
+            </Row>
+            <Row justify="flex-start">
+              <Text
+                className="styledTitle"
+                css={{ mb: 0, '@xsMax': { d: 'inline-block' } }}
+              >
+                matchen.
+              </Text>
+            </Row>
+
+            <Text
+              h4
+              color="#666"
+              weight="medium"
+              css={{ mt: 30 }}
+              className="comment"
+            >
+              Spesifiser interessen deres, og finn likesinnede. Bruk også
+              filtreringsverktøyene for det de er verdt!
+            </Text>
+          </div>
+        </div>
+
+        <Spacer />
       </div>
     </div>
   );

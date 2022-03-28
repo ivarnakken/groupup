57dd0406ce0f5c4a96bfb4d23dbb3ddb13a4f0aa
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-03-28 22:11:48 +0200

    #42 Added group filter

--- app/src/components/GroupList/index.js
+++ app/src/components/GroupList/index.js
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Group from '../Group';
-import { Text } from '@nextui-org/react';
+import { Text, Input, Row, Spacer } from '@nextui-org/react';
 import './style.css';
 
 const GroupList = () => {
   const { isLoggedIn } = useSelector((state) => state.auth);
 
   const [allGroups, setAllGroups] = useState([]);
+  const [wordFilterValue, setWordFilterValue] = useState('');
   // const [likedGroups, setLikedGroups] = useState([]);
 
   useEffect(async () => {
@@ -18,6 +19,15 @@ const GroupList = () => {
     });
   }, []);
 
+  const handleWordFilterChange = (event) => {
+    setWordFilterValue(event.target.value);
+  };
+
+  const filterGroups = (group) =>
+    (group.description + group.name + group.leader.username)
+      .toLowerCase()
+      .includes(wordFilterValue.toLowerCase());
+
   return (
     <div className="groupList">
       <div className="header">
@@ -25,23 +35,41 @@ const GroupList = () => {
           Grupper
         </Text>
       </div>
+      <Spacer />
+      <Row justify="center">
+        <Input
+          type="text"
+          name="name"
+          labelPlaceholder="Søk i grupper"
+          value={wordFilterValue}
+          onChange={handleWordFilterChange}
+          clearable
+          underlined
+        />
+      </Row>
 
       <div className="list">
-        {allGroups.map((group) => {
-          return (
-            <Group
-              key={group._id}
-              id={group._id}
-              name={group.name}
-              leader={group.leader}
-              members={group.members}
-              image={group.image}
-              description={group.description}
-              className="groups"
-              onClick={() => console.log(group._id)}
-            />
-          );
-        })}
+        {allGroups
+          .filter(filterGroups)
+          .map((group) => {
+            return (
+              <Group
+                key={group._id}
+                id={group._id}
+                name={group.name}
+                leader={group.leader}
+                members={group.members}
+                image={group.image}
+                description={group.description}
+                className="groups"
+                onClick={() => console.log(group._id)}
+              />
+            );
+          })
+          .reduce(
+            (previousValue, currentValue, currentIndex, array) => array,
+            <Text>Ingen grupper her :(</Text>
+          )}
       </div>
 
       {isLoggedIn && (
